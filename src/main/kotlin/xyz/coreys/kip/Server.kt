63378bf2509d6b67194f7efc6cf61c590e82f79e
Corey <cpotts97@live.co.uk>
2018-03-30 19:09:06 +0100

    Fixed method to get file extension and removed some old code

--- src/main/kotlin/xyz/coreys/kip/Server.kt
+++ src/main/kotlin/xyz/coreys/kip/Server.kt
@@ -3,10 +3,9 @@ package xyz.coreys.kip
 import spark.kotlin.*
 import xyz.coreys.kip.utils.Config
 import xyz.coreys.kip.utils.generateFileName
-import java.net.URLConnection
 import java.nio.file.Files
 import java.nio.file.Paths
-import javax.servlet.MultipartConfigElement
+
 
 object Server {
 	
@@ -26,13 +25,11 @@ object Server {
 		// TODO secret token/password
 		
 		post("/${Config.uploadUrl}") {
-			request.attribute("org.eclipse.jetty.multipartConfig", MultipartConfigElement("/temp"))
-			
-			var fileName = ""
+			var fileName = request.raw().getPart(Config.formName).submittedFileName
+			val extension = fileName.substring(fileName.lastIndexOf('.') + 1, fileName.length)
 			
 			request.raw().getPart(Config.formName).inputStream.use({ input ->
-				val fileExtension = URLConnection.guessContentTypeFromStream(input)
-				fileName = generateFileName(".$fileExtension")
+				fileName = generateFileName(".$extension")
 				Files.write(Paths.get(Config.uploadDirectory, fileName), input.readBytes())
 			})
 			
